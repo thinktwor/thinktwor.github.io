7363bf52867c70570e38b1831d414c3eaba3ed7d
tworitdash <tworitdash@gmail.com>
2015-06-21 03:09:58 +0530

    Static

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ jekyll build --source . --destination _deploy
 touch _deploy/CNAME
 echo 'blog.tworitdash.in' > _deploy/CNAME
 cd _deploy
-git init 
+ 
 git add .
 git commit -m "Static"
-git remote add origin https://github.com/thinktwor/thinktwor.github.io.git
+
 git push -u origin master 
 cd ..
 git add .
