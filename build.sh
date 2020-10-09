7ec81730cd4bcfea6a373fdf4bf664699a8d7279
Tworit Dash <tworitdash@x080155.tbm-m.tudelft.nl>
2020-10-09 19:54:36 +0200

    Source Added

--- build.sh
+++ build.sh
@@ -2,12 +2,17 @@ jekyll build --source . --destination _deploy
 touch _deploy/CNAME
 echo 'blog.tworitdash.in' > _deploy/CNAME
 cd _deploy
- 
+
+git checkout master
+
 git add .
 git commit -m "Static"
 
 git push -u origin master 
+
 cd ..
+
+git checkout branch
 
 git add .
 git commit -m "Source Added"
