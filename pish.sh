ff6ffe37a5079846d86ec78b295a35e249d35084
Tworit Dash <tworitdash@x080155.tbm-m.tudelft.nl>
2020-10-09 20:04:40 +0200

    Source Added

---
+++ pish.sh
@@ -0,0 +1,14 @@
+bundle exec jekyll build --source . --destination _deploy
+touch _deploy/CNAME
+echo 'blog.tworitdash.in' > _deploy/CNAME
+cd _deploy
+ 
+git add .
+git commit -m "Static"
+
+git push -u origin master 
+cd ..
+
+git add .
+git commit -m "Source Added"
+git push origin branch
